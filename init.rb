f261ed3a394340e2cad95121bed5790a20d9c72b
Andriy Lesyuk <s-andy@andriylesyuk.com>
2019-12-13 12:24:40 +0200

    Minor fixes

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Rails.logger.info 'Starting That Issue Reply Button plugin for Redmine'
 
 Redmine::Plugin.register :that_issue_reply_button do
     name 'That Issue Reply Button'
-    author 'Andriy Lesyuk'
+    author 'Andriy Lesyuk for That Company'
     author_url 'http://www.andriylesyuk.com/'
     description 'Adds "Reply" button to the action menu of the issue page.'
     url 'https://github.com/thatcompany/that_issue_reply_button'
